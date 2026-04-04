e115205eeead83fcc5f2a697b1705ef1524e4009
Francis Kuefler <ftkuefler@gmail.com>
2026-04-04 12:19:45 -0500

    2218A - The 67th Integer Problem (Round 1090 Div. 4)

---
+++ 2200-2299/2218A_The_67_th_Integer_Problem.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t, x;
+    cin >> t;
+
+    while (t--) {
+        cin >> x;
+        cout << x << '\n';
+    }
+}
